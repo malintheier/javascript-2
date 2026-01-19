30e25343a286942ffc2ea06ff28138466f2e674e
malintheier <malhei01980@stud.noroff.no>
2026-01-19 22:08:53 +0100

    Add registration form handler and API integration
    Introduces main.js to handle registration form submission and send user data to the API. Also links main.js to login and register pages. Removes the 'name' attribute from the avatar input in register.html.

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const registerForm = document.querySelector("#registerForm");
+
+const BASE_API_URL = "https://v2.api.noroff.dev";
+const AUTH_REGISTER_URL = `${BASE_API_URL}/auth/register`;
+
+async function registerUser(userDetails) {
+  try {
+    const fetchOptions = {
+      method: "POST",
+      body: JSON.stringify(userDetails),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch(AUTH_REGISTER_URL, fetchOptions);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function onRegisterFormSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const formFields = Object.fromEntries(formData);
+  registerUser(formFields);
+}
+registerForm.addEventListener("submit", onRegisterFormSubmit);
